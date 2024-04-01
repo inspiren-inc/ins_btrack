081d3d4cc7ba90093f2180e7a6c3d1ac10b40a83
Wassim El Ahmar <w.elahmar@hotmail.com>
2024-04-01 16:40:44 -0400

    removed unnecessary imports

--- yolox/utils/__init__.py
+++ yolox/utils/__init__.py
@@ -2,15 +2,15 @@
 # -*- coding:utf-8 -*-
 # Copyright (c) 2014-2021 Megvii Inc. All rights reserved.
 
-from .allreduce_norm import *
-from .boxes import *
-from .checkpoint import load_ckpt, save_checkpoint
+#from .allreduce_norm import *
+#from .boxes import *
+#from .checkpoint import load_ckpt, save_checkpoint
 from .demo_utils import *
-from .dist import *
-from .ema import ModelEMA
+#from .dist import *
+#from .ema import ModelEMA
 from .logger import setup_logger
 from .lr_scheduler import LRScheduler
-from .metric import *
-from .model_utils import *
+#from .metric import *
+#from .model_utils import *
 from .setup_env import *
 from .visualize import *
